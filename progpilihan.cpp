c8853475b63514c4d9c7e3843f0b7c561cd5bad9
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2017-11-20 13:50:54 +0800

    Variabel dan Output pilihan

--- progpilihan.cpp
+++ progpilihan.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 int main(){
+	int panjang, lebar, luas ,pilihan, alas, tinggi;
+	int gpkk,jk;
+	int lbr,gt,pjk,gk;
+	int x;
+	double Tinggi, berat;
+	char yt;
+	atas:
 	
+	cout <<"1.Persegi panjang		|\n";
+	cout <<"2.Segitiga			|\n";
+	cout <<"3.Perhitungan berat badan	|\n";
+	cout <<"4.Penentuan Nilai		|\n";
+	cout <<"5.Perhitungan Gaji Karyawan	|\n";
+	cout <<"---------------------------------\n";
+	cout <<"Masukan Pilihan =";cin >>pilihan;	
 }
