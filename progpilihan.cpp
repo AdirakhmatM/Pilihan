5966f2ef6451f1900a7b7e90c23f5569e36147ff
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2017-11-20 13:52:28 +0800

    Membuat program pilihan dan input output nilai pilihan program

--- progpilihan.cpp
+++ progpilihan.cpp
@@ -17,5 +17,96 @@ int main(){
 	cout <<"4.Penentuan Nilai		|\n";
 	cout <<"5.Perhitungan Gaji Karyawan	|\n";
 	cout <<"---------------------------------\n";
-	cout <<"Masukan Pilihan =";cin >>pilihan;	
+	cout <<"Masukan Pilihan =";cin >>pilihan;
+	
+	switch(pilihan){
+		case 1:
+		cout<<"masukan panjang =";cin>>panjang;
+		cout<<"masukan lebar =";cin>>lebar;
+		luas =panjang * lebar;
+		cout<<"luas persegi panjang="<<luas<<"\n";
+		cout<<"Kembali..?? [Y/T]";cin>>yt;
+		if(yt=='Y'||yt=='y')
+		goto atas;
+		else if(yt=='T'||yt=='t')
+		cout<<"\n";
+		cout<<"Terima Kasih"<<endl;
+		break;
+		case 2:
+		cout<<"Masukkan panjang alas Segitiga=";cin>>alas;
+		cout<<"Masukkan tinggi Segitiga=";cin>>tinggi;
+		luas=0.5*alas*tinggi;
+		cout<<"luas Segitiga ="<<luas<<"\n";
+		cout<<"Kembali..?? [Y/T]";cin>>yt;
+		if(yt=='Y'||yt=='y')
+		goto atas;
+		else if(yt=='T'||yt=='t')
+		cout<<"\n";
+		cout<<"Terima Kasih"<<endl;
+		break;
+		case 3:
+		cout <<"Perhitungan berat badan\n";
+		cout <<"Masukkan berat badan : ";cin >>berat;
+		cout <<"Masukkan tinggi badan : ";cin >>Tinggi;
+	
+		cout <<"Hasil \n";
+		cout <<"Berat badan : "<<berat<<endl;
+		cout <<"Tinggi badan : "<<Tinggi<<endl;
+	
+		if (berat<Tinggi/2.5){
+		cout <<"Underweight";
+		}else if (Tinggi/2.3<berat){
+		cout <<"Overweight";
+		}else if (Tinggi/2.5 <=berat <=Tinggi/2.3){
+		cout <<"Normal!!";
+		}
+		cout<<"\n";
+		cout<<"Kembali..?? [Y/T]";cin>>yt;
+		if(yt=='Y'||yt=='y')
+		goto atas;
+		else if(yt=='T'||yt=='t')
+		cout<<"\n";
+		cout<<"Terima Kasih"<<endl;
+		break;
+		case 4:
+		cout <<"Masukkan Nilai : ";cin >>x;
+	
+		if (x>0){
+		cout <<"Nilai positif";
+		}else if (x<0){
+		cout <<"Nilai negatif";
+		}else {
+		cout <<"Nilai nol !!";
+		}
+		cout<<"\n";
+		cout<<"Kembali..?? [Y/T]";cin>>yt;
+		if(yt=='Y'||yt=='y')
+		goto atas;
+		else if(yt=='T'||yt=='t')
+		cout<<"\n";
+		cout<<"Terima Kasih"<<endl;
+		break;
+		case 5:
+		cout <<"Perhitungan Gaji Karyawan "<<endl;
+		cout <<"Masukkan Gaji Pokok : Rp. ";cin >>gpkk;
+		cout <<"Masukkan Jam Kerja : ";cin >>jk;
+		lbr=jk-40;
+		gt=(lbr*1.5+40)*gpkk;
+		pjk=gt*0.15;
+		gk=gt-pjk;
+	
+		cout <<"Hasil Perhitungan Gaji"<<endl;
+		cout <<"Gaji Pokok	: Rp. "<<gpkk<<endl;
+		cout <<"Jam Kerja 	: "<<jk<<endl;
+		cout <<"Jam Lembur	: "<<lbr<<endl;
+		cout <<"Gaji sebelum dipotong pajak : Rp. "<<gt<<endl;
+		cout <<"Total Gaji 	: Rp. "<<gk;
+		cout<<"\n";
+		cout<<"Kembali..?? [Y/T]";cin>>yt;
+		if(yt=='Y'||yt=='y')
+		goto atas;
+		else if(yt=='T'||yt=='t')
+		cout<<"\n";
+		cout<<"Terima Kasih"<<endl;
+	}	
 }
